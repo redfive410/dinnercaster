5d614e06e1b3a8a9bdc293f514fb0da345f42dbd
Armitage <jarmitage@SDGL154381ee0.local>
2017-03-01 17:06:54 -0800

    WIP

--- api/lambda/initDynamofunc.js
+++ api/lambda/initDynamofunc.js
@@ -11,48 +11,28 @@ var fun = function(event, context)
             {
                 PutRequest: {
                     Item: {
-                        "SuperHero": "Batman",
-			"Villain1": "Joker",
-			"Villain2": "Bane",
-			"Villain3": "Ras Al Ghul",
-			"MissionStatus": "In Progress",
-			"SecretIdentity": "Bruce Wayne"
+                        "DinnerName": "Perogies"
                     }
                 }
             },
             {
                 PutRequest: {
                     Item: {
-                         "SuperHero": "Superman",
-			 "Villain1": "Doomsday",
-			 "Villain2": "General Zod",
-			 "Villain3": "Lex Luthor",
-			 "MissionStatus": "In progress",
-			 "SecretIdentity": "Clark Kent"
+                         "DinnerName": "Ramen"
                     }
                 }
             },
             {
                 PutRequest: {
                     Item: {
-                        "SuperHero": "The Winchester Brothers",
-			"Villain1": "Vampires",
-			"Villain2": "Ghosts",
-			"Villain3": "Werewolves",
-			"MissionStatus": "Complete",
-			"SecretIdentity": "Sam and Dean"
+                        "DinnerName": "Thai"
                     }
                 }
             },
 	    {
                 PutRequest: {
                     Item: {
-                        "SuperHero": "Iron Man",
-			"Villain1": "Apocalypse",
-			"Villain2": "Doctor Doom",
-			"Villain3": "LOki",
-			"MissionStatus": "In progress",
-			"SecretIdentity": "Tony Stark"
+                        "DinnerName": "Tacos"
                     }
                 }
             }
