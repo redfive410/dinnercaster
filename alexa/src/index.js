36341dab4dc5c69c14708120fd3419b4c8cc87da
Armitage <jarmitage@SDGL1536e5bc4.local>
2017-05-05 13:43:35 -0700

    v0.3

--- alexa/src/index.js
+++ alexa/src/index.js
@@ -2,11 +2,9 @@
  * This sample demonstrates a simple skill built with the Amazon Alexa Skills
  * nodejs skill development kit.
  **/
-
 'use strict';
 
 const Alexa = require('alexa-sdk');
-
 const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
 const languageStrings = {
@@ -26,6 +24,15 @@ const languageStrings = {
     }
 };
 
+exports.handler = function(event, context, callback) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
 const handlers = {
     'LaunchRequest': function () {
         this.emit('GetDinner');
@@ -37,15 +44,16 @@ const handlers = {
         // Get a random dinner from the dinner list
         // Use this.t() to get corresponding language data
         const dinnerArr = this.t('DINNERS');
-
         console.log(dinnerArr);
-
         const dinnerIndex = Math.floor(Math.random() * dinnerArr.length);
         const randomDinner = dinnerArr[dinnerIndex];
 
-        // Create speech output
-        const speechOutput = this.t('GET_DINNER_MESSAGE') + randomDinner;
-        this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomDinner);
+        readDynamoItems(null, response=>{
+          console.log(response);
+          // Create speech output
+          const speechOutput = this.t('GET_DINNER_MESSAGE') + randomDinner;
+          this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomDinner);
+        });
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = this.t('HELP_MESSAGE');
@@ -63,11 +71,22 @@ const handlers = {
     },
 };
 
-exports.handler = (event, context) => {
-    const alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
+function readDynamoItems(params, callback) {
+  // Get a random better dinner idea
+  var AWS = require('aws-sdk');
+  AWS.config.region = 'us-east-1';
+
+  var lambda = new AWS.Lambda();
+  lambda.invoke({
+      FunctionName: 'arn:aws:lambda:us-east-1:109613526816:function:dinnercaster-1-getdinnerslistFunction-1HRPVH217WRS2',
+      InvocationType: 'RequestResponse',
+      Payload: '{}'
+  }, (err, data) => {
+    if (err) {
+           console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+       } else {
+           console.log("GetItems succeeded:", JSON.stringify(data, null, 2));
+           callback(data.Payload);
+       }
+  });
+}
