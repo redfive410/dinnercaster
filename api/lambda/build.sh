bc44dfe1d5a487a745f61285ed47aa1a06782091
Armitage <mrjimmy410@gmail.com>
2017-05-09 17:41:33 -0700

    WIP

---
+++ api/lambda/build.sh
@@ -0,0 +1,6 @@
+zip -9 -r initDynamoDBfunc.js.zip initDynamoDBfunc.js
+zip -9 -r getdinnerslistfunc.js.zip getdinnerslistfunc.js
+zip -9 -r getdinnerdetailsfunc.js.zip getdinnerdetailsfunc.js
+
+#TODO
+# upload to S3 bucket
